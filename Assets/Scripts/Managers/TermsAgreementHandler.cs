f2ae2051a44d71352a8ebc14b45842eb3473c847
jeiiiiiii <jeirenmondejar08@gmail.com>
2025-10-08 12:14:40 +0800

    TermsAndConditionDone

--- Assets/Scripts/Managers/TermsAgreementHandler.cs
+++ Assets/Scripts/Managers/TermsAgreementHandler.cs
@@ -1,56 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.UI;
-
-// public class TermsAgreementHandler : MonoBehaviour
-// {
-//     [Header("UI References")]
-//     public Toggle agreementToggle;
-//     public GameObject termsPopup;
-//     public Button agreeButton;
-//     public ScrollRect scrollRect; // 👈 Add this
-
-//     private bool hasShownPopup = false;
-//     private bool hasReachedBottom = false;
-
-//     void Start()
-//     {
-//         termsPopup.SetActive(false);
-//         agreementToggle.onValueChanged.AddListener(OnToggleChanged);
-//         agreeButton.onClick.AddListener(OnAgreeClicked);
-
-//         // Initially disable the Agree button
-//         agreeButton.interactable = false;
-
-//         // Listen to scroll changes
-//         scrollRect.onValueChanged.AddListener(OnScrollChanged);
-//     }
-
-//     void OnToggleChanged(bool isOn)
-//     {
-//         if (isOn && !hasShownPopup)
-//         {
-//             termsPopup.SetActive(true);
-//             agreementToggle.isOn = false;
-//         }
-//     }
-
-//     void OnScrollChanged(Vector2 scrollPos)
-//     {
-//         // scrollRect.verticalNormalizedPosition = 1 when top, 0 when bottom
-//         if (scrollRect.verticalNormalizedPosition <= 0.01f && !hasReachedBottom)
-//         {
-//             hasReachedBottom = true;
-//             agreeButton.interactable = true; // Enable button
-//         }
-//     }
-
-//     void OnAgreeClicked()
-//     {
-//         hasShownPopup = true;
-//         termsPopup.SetActive(false);
-//         agreementToggle.isOn = true;
-//     }
-// }
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -63,7 +10,7 @@ public class TermsAgreementHandler : MonoBehaviour
     public ScrollRect scrollRect;
 
     private bool hasReachedBottom = false;
-    private bool isAgreeProcess = false; // 🔹 Flag to detect system-initiated toggle change
+    private bool isAgreeProcess = false; // Flag to detect system-initiated toggle change
 
     void Start()
     {
@@ -77,22 +24,23 @@ public class TermsAgreementHandler : MonoBehaviour
 
     void OnToggleChanged(bool isOn)
     {
-        // Ignore event if we're programmatically changing toggle during the agree process
+        // Ignore programmatic toggle changes
         if (isAgreeProcess)
             return;
 
-        // Always show popup when user tries to check or uncheck
-        if (isOn || !isOn)
+        // Only show popup when user tries to CHECK the toggle
+        if (isOn)
         {
             // Reset scroll + disable agree button
             scrollRect.verticalNormalizedPosition = 1f;
             agreeButton.interactable = false;
             hasReachedBottom = false;
 
-            // Open popup and uncheck toggle until they agree
+            // Open popup and uncheck toggle until they fully agree
             termsPopup.SetActive(true);
             agreementToggle.isOn = false;
         }
+        // No popup when unchecking
     }
 
     void OnScrollChanged(Vector2 scrollPos)
@@ -110,7 +58,7 @@ public class TermsAgreementHandler : MonoBehaviour
         // Close popup and set toggle to checked
         termsPopup.SetActive(false);
 
-        // Temporarily mark this as system-driven
+        // Temporarily mark this as system-driven to prevent re-trigger
         isAgreeProcess = true;
         agreementToggle.isOn = true;
         isAgreeProcess = false;
